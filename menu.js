72b23736fdce153a5b05fd33288b84d3d843aa10
khushbu <rahuljai8267@gmail.com>
2020-11-10 17:20:11 +0530

    first commit

---
+++ menu.js
@@ -0,0 +1,8 @@
+// alert("hi i am alert")
+
+function openSubMenu1(){
+    document.getElementById("product").style="width : 205px ";
+}
+function openSubMenu2(){
+    document.getElementById("emi-network-1").style="width : 205px ";
+}
