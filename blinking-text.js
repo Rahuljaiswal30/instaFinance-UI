6d31faedda36d3f0d69bd4292e6627f8065ccb54
khushbu <rahuljai8267@gmail.com>
2020-12-03 12:07:44 +0530

    removed the @media scree responsive features

---
+++ blinking-text.js
@@ -0,0 +1,13 @@
+// alert("connecting");
+
+var blink = document.querySelectorAll(".blink")
+var textColor = true;
+setInterval( function(){ 
+    if(textColor){
+        blink.style.color = "red";
+    }else{
+        blink.style.color = "purple";
+    }
+    textColor = !textColor;
+        
+}, 1000)
